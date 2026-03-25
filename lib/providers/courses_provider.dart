22da0f127bf0a501ed83decc0f77728c55683a5f
Abdurahmon <abdurrohmanmominov@gmail.com>
2026-03-25 17:27:35 +0500

    asdb

--- lib/providers/courses_provider.dart
+++ lib/providers/courses_provider.dart
@@ -25,7 +25,7 @@ class CoursesNotifier extends StateNotifier<AsyncValue<List<CourseModel>>> {
         title: 'Alifbo - 1-dars (A, B, T harflari)',
         description:
             'Arab alifbosi bilan tanishishni boshlaymiz. Ushbu darsda 3 ta harfni o\'rganamiz.',
-        youtubeVideoId: 'S2OWe0M0KCE', // Random Arabic alphabet video ID
+        youtubeVideoId: '7UhChJPJZ9HnE97n', // Random Arabic alphabet video ID
         transcript:
             'Arab alifbosida jami 28 ta harf mavjud. Bugun biz Alif (A), Ba (B), va Ta (T) harflarini ko\'rib chiqamiz...',
         unitName: 'Unit 1: Alifbo',
@@ -34,7 +34,7 @@ class CoursesNotifier extends StateNotifier<AsyncValue<List<CourseModel>>> {
         id: 'l2',
         title: 'Alifbo - 2-dars',
         description: 'Harflarni o\'rganishda davom etamiz.',
-        youtubeVideoId: 'f0g7S-nC0D0',
+        youtubeVideoId: 'Og5anIb_FUBgv7qx',
         transcript:
             'Oldingi darsda o\'tilganlarni takrorlab, navbatdagi harflarga o\'tamiz...',
         unitName: 'Unit 1: Alifbo',
